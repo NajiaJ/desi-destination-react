7aea481700a45ae88d71875204043a68407e7cd0
Najia Jahan <najia.jahan51@myhunter.cuny.edu>
2025-05-02 00:36:38 -0400

    adding basename for router

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/desi-destination-react">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
